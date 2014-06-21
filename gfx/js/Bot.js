65420a635660f89f038e374682b8b11a4545c57c
Christophe FEIJOO <cfeijoo@e2r4p2.42.fr>
2014-06-21 17:56:18 +0200

    Fix not displaying Bot meshes

--- gfx/js/Bot.js
+++ gfx/js/Bot.js
@@ -1,6 +1,6 @@
 var Bot = function(game, number, x, y, orientation, level, team) {
 	var self = this;
- 
+
 	this.name = number;
 	this.x;
 	this.y;
@@ -8,7 +8,11 @@ var Bot = function(game, number, x, y, orientation, level, team) {
 	this.level = level;
 	this.team = team;
 
+	// console.log('Creating BOT');
+
 	var block = game.map.blocks[x][y];
+
+	// console.log(block)
 
 	this.position = {
 		x: block.position.x,
@@ -26,23 +30,24 @@ var Bot = function(game, number, x, y, orientation, level, team) {
 
 		// Mesh position update
 		self.mesh.position.set(block.position.x, 0.3, block.position.y);
-		self.mesh.updateMatrix();
-
 		self.mesh.rotation.set(Math.PI/2, 0, Math.PI/2 * (4 + (o - 3)));
+		self.mesh.updateMatrix();
 	}
 
 	function createMesh() {
 
+		console.log('Creating Bot Mesh')
+
 		self.mesh = new THREE.Mesh(
-			new THREE.CylinderGeometry(0, 0.4, 0.4, 4),
+			new THREE.CylinderGeometry(0, 0.2, 0.2, 4),
 			game.materials.basic);
 
 		self.mesh.scale.x = 0.4;
 		self.mesh.scale.z = 0.1;
 
+		self.setPosition(x, y, orientation);
 		game.scene.add(self.mesh);
 
-		self.setPosition(block.position.x, block.position.y, orientation);
 		self.mesh.onclick = function(e, pick) {
 			// var pickedMesh = pick.pickedMesh;
 			console.log(self);
--- gfx/js/ResponseParser.js
+++ gfx/js/ResponseParser.js
@@ -28,7 +28,7 @@ var ResponseParser = function(client, game) {
 			return parseInt(this[i]);
 		}
 
-		console.log(a)
+		// console.log(a)
 
 		switch (a[0]) {
 
@@ -48,12 +48,16 @@ var ResponseParser = function(client, game) {
 				break;
 
 			case 'pnw':
+				// console.log(a);
 				var player = parseInt(a[1].replace('#', ''));
 				game.createBot(player, a.i(2), a.i(3), a.i(3), a.i(4), a[5]);
 				break;
 
 			case 'ppo':
 				var player = parseInt(a[1].replace('#', ''));
+				// console.log('Setting position')
+				if (game.bots[player])
+					console.log(game.bots[player])
 				game.bots[player].setPosition(a.i(2), a.i(3), a.i(4));
 				break;
 
